66fbf14704fd9433cc15c8cad8e399c3b2d2449d
Dennis Plotnikov <d.y.plotnikov@gmail.com>
2024-06-11 12:35:25 -0400

    Split the classification and regression into two models, final test uses the predictions made by the classification model to aid the regression

--- wa_hls4ml_model.py
+++ wa_hls4ml_model.py
@@ -3,26 +3,51 @@ from tensorflow.keras.layers import Dense, Activation, BatchNormalization, Dropo
 from tensorflow.keras.regularizers import l1
 
 
-def create_model():
+def create_model_classification():
     model = Sequential()
     # d_in	d_out prec rf strategy (one-hot encoded)
-    model.add(Dense(64, input_shape=(6,), name='fc1', kernel_initializer='lecun_uniform', kernel_regularizer=l1(0.0001)))
+    model.add(Dense(128, input_shape=(6,), name='fc1', kernel_initializer='lecun_uniform', kernel_regularizer=l1(0.00001)))
+    model.add(Activation(activation='elu', name='elu'))
+
+    model.add(Dense(2048, name='fc2', kernel_initializer='lecun_uniform', kernel_regularizer=l1(0.00001)))
     model.add(Activation(activation='leaky_relu', name='relu1'))
-    #model.add(BatchNormalization())
-    model.add(Dense(128, name='fc2', kernel_initializer='lecun_uniform', kernel_regularizer=l1(0.0001)))
+    model.add(Dropout(0.3))
+
+    model.add(Dense(4096, name='fc3', kernel_initializer='lecun_uniform', kernel_regularizer=l1(0.000001)))
     model.add(Activation(activation='leaky_relu', name='relu2'))
-    #model.add(BatchNormalization())
-    #model.add(Dropout(0.4))
-    model.add(Dense(64, name='fc3', kernel_initializer='lecun_uniform', kernel_regularizer=l1(0.0001)))
+    model.add(Dropout(0.5))
+
+    model.add(Dense(2048, name='fc4', kernel_initializer='lecun_uniform', kernel_regularizer=l1(0.000001))) #32
     model.add(Activation(activation='leaky_relu', name='relu3'))
-    #model.add(BatchNormalization())
-    model.add(Dense(128, name='fc4', kernel_initializer='lecun_uniform', kernel_regularizer=l1(0.0001))) #32
-    model.add(Activation(activation='leaky_relu', name='relu4'))
-    #model.add(BatchNormalization())
-    #model.add(Dropout(0.2))
-    model.add(Dense(64, name='fc5', kernel_initializer='lecun_uniform', kernel_regularizer=l1(0.0001))) #32
-    model.add(Activation(activation='leaky_relu', name='relu5'))
-    #hls_synth_success, WorstLatency_hls, IntervalMax_hls, FF_hls, LUT_hls, BRAM_18K_hls, DSP_hls
-    model.add(Dense(7, name='output', kernel_initializer='lecun_uniform', kernel_regularizer=l1(0.0001)))
+    model.add(Dropout(0.3))
+
+    model.add(Dense(128, name='fc6', kernel_initializer='lecun_uniform', kernel_regularizer=l1(0.00001))) #32
+    model.add(Activation(activation='elu', name='elu2'))
+
+    #hls_synth_success
+    model.add(Dense(1, name='output', kernel_initializer='lecun_uniform', kernel_regularizer=l1(0.001)))
     return model
 
+def create_model_regression():
+    model = Sequential()
+    # d_in	d_out prec rf strategy (one-hot encoded)
+    model.add(Dense(64, input_shape=(7,), name='fc1', kernel_initializer='lecun_uniform', kernel_regularizer=l1(0.00001)))
+    model.add(Activation(activation='leaky_relu', name='relu1'))
+    #model.add(BatchNormalization())
+    model.add(Dense(1024, name='fc2', kernel_initializer='lecun_uniform', kernel_regularizer=l1(0.00001)))
+    model.add(Activation(activation='leaky_relu', name='relu2'))
+    #model.add(BatchNormalization())
+    model.add(Dropout(0.5))
+    model.add(Dense(4096, name='fc3', kernel_initializer='lecun_uniform', kernel_regularizer=l1(0.00001)))
+    model.add(Activation(activation='elu', name='elu1'))
+    #model.add(BatchNormalization())
+    model.add(Dropout(0.7))
+    model.add(Dense(1024, name='fc4', kernel_initializer='lecun_uniform', kernel_regularizer=l1(0.00001))) #32
+    model.add(Activation(activation='leaky_relu', name='relu3'))
+    #model.add(BatchNormalization())
+    model.add(Dropout(0.5))
+    model.add(Dense(64, name='fc6', kernel_initializer='lecun_uniform', kernel_regularizer=l1(0.00001))) #32
+    model.add(Activation(activation='elu', name='elu2'))
+    #hls_synth_success, WorstLatency_hls, IntervalMax_hls, FF_hls, LUT_hls, BRAM_18K_hls, DSP_hls
+    model.add(Dense(6, name='output', kernel_initializer='lecun_uniform', kernel_regularizer=l1(0.001)))
+    return model
--- wa_hls4ml_train.py
+++ wa_hls4ml_train.py
@@ -21,9 +21,16 @@ import csv
 import json
 import pickle
 
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+import numpy as np
+import matplotlib.pyplot as plt
+
 import tensorflow as tf
+
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.optimizers import Adam, AdamW
+from tensorflow.keras.models import load_model
+
 import keras
 import keras.backend as K
 from sklearn.metrics import roc_auc_score, confusion_matrix, accuracy_score, ConfusionMatrixDisplay
@@ -31,6 +38,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 import wa_hls4ml_model
+
 from callbacks import all_callbacks
 
 if os.system('nvidia-smi') == 0:
@@ -122,7 +130,7 @@ def preprocess_features(data, binary_feature_names, numeric_feature_names, categ
 
     return preprocessed_data
 
-def plot_loss(history):
+def plot_loss(name, history):
     plt.figure(figsize=(10,6))
     plt.plot(history.history['loss'], label='Training Loss')
     plt.plot(history.history['val_loss'], label='Validation Loss')
@@ -130,91 +138,28 @@ def plot_loss(history):
     plt.ylabel('Loss')
     plt.xlabel('Epoch')
     plt.legend(loc='upper right')
-    plt.savefig('train_loss.png')
+    plt.savefig(name+'_train_loss.png')
     plt.show()
 
-if __name__ == "__main__":
+def plot_histograms(y_predicted, y_actual, X_raw):
+    plt.figure(figsize=(10,6))
 
-    #dataset, preprocessors = preprocess('results/results_format_test.csv')
-    #X, y = dataset[0], dataset[1]
-    #preprocessor_x, preprocessor_y = preprocessors[0], preprocessors[1]
-
-    input_features = ["d_in", "d_out", "prec", "rf", "strategy"]
-    output_features = ["WorstLatency_hls", "IntervalMax_hls", "FF_hls", "LUT_hls", "BRAM_18K_hls",
-                       "DSP_hls","hls_synth_success"]
-    binary_feature_names = ['hls_synth_success']
-    numeric_feature_names = ["d_in", "d_out", "prec", "rf", "WorstLatency_hls", "IntervalMax_hls", "FF_hls", "LUT_hls",
-                             "BRAM_18K_hls", "DSP_hls"]
-    categorical_feature_names = ["strategy"]
-
-    X, y, X_raw = preprocess('results/results_format_test.csv', input_features, output_features,
-                             binary_feature_names, numeric_feature_names,
-                             categorical_feature_names)
-
-
-    # Split the data 70 - 20 - 10 train test val
-    # Train and test
-    print(X.shape, y.shape)
-    print("X Data: ",input_features)
-    print(X)
-    print("Y Data: ",output_features)
-    print(y[0])
-    X_train, X_test, y_train, y_test, X_raw_train, X_raw_test = sklearn.model_selection.train_test_split(X, y, X_raw,  test_size=0.2, random_state=42, shuffle=True)
-    #X_train, X_test, y_train, y_test = map(tf.convert_to_tensor, (X_train, X_test, y_train, y_test))
+    y_difference = y_actual - y_predicted
+    y_abs_diff = np.abs(y_difference)
 
 
 
-    train = False
-    mpl_plots = False
+    plt.plot(history.history['loss'], label='Training Loss')
+    plt.plot(history.history['val_loss'], label='Validation Loss')
+    plt.title('Model loss')
+    plt.ylabel('Loss')
+    plt.xlabel('Epoch')
+    plt.legend(loc='upper right')
+    plt.savefig(name+'_train_loss.png')
+    plt.show()
 
-    model = wa_hls4ml_model.create_model()
 
-
-    if train:
-        adam = Adam(lr=0.0001)
-        model.compile(optimizer=adam, loss=tf.keras.losses.LogCosh(), metrics=['mse', 'mae'])
-        callbacks = all_callbacks(
-            stop_patience=1000,
-            lr_factor=0.5,
-            lr_patience=10,
-            lr_epsilon=0.000001,
-            lr_cooldown=2,
-            lr_minimum=0.0000001,
-            outputDir='model_1',
-        )
-        history = model.fit(
-            X_train,
-            y_train,
-            batch_size=1024,
-            epochs=200,
-            validation_split=0.125,
-            shuffle=True,
-            callbacks=callbacks.callbacks,
-        )
-        plot_loss(history)
-
-    else:
-        from tensorflow.keras.models import load_model
-
-        model = load_model('model_1/KERAS_check_best_model.h5')
-
-    from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
-    import numpy as np
-    import matplotlib.pyplot as plt
-
-    # Predict the output for X_test
-    y_pred = model.predict(X_test)
-
-    # Calculate metrics
-    mae = mean_absolute_error(y_test, y_pred)
-    mse = mean_squared_error(y_test, y_pred)
-    rmse = np.sqrt(mse)
-    r2 = r2_score(y_test, y_pred)
-
-    print(f'Mean Absolute Error (MAE): {mae}')
-    print(f'Mean Squared Error (MSE): {mse}')
-    print(f'Root Mean Squared Error (RMSE): {rmse}')
-    print(f'R-squared (R2 Score): {r2}')
+def plot_results(name, mpl_plots, y_test, y_pred, X_raw_test, output_features):
 
     colors = ['blue', 'green', 'red', 'cyan', 'magenta', 'yellow', 'orange']
 
@@ -317,7 +262,233 @@ if __name__ == "__main__":
     # Set the layout
     fig.update_layout(height=1900, width=1900, title='wa-hls4ml 1-Layer Dense Toy Model - Actual vs Predicted')
 
-    pio.write_html(fig, file='plots/wa-hls4ml_outputs.html', auto_open=False)
-    py.plot(fig, filename='wa-hls4ml_outputs', auto_open=True)
+    pio.write_html(fig, file='plots/'+name+'_wa-hls4ml_outputs.html', auto_open=False)
+    # py.plot(fig, filename='wa-hls4ml_outputs', auto_open=False)
     # Show the plot
-    fig.show()
+    # fig.show()
+
+
+def preprocess_data():
+    input_features = ["d_in", "d_out", "prec", "rf", "strategy", "hls_synth_success"]
+    output_features = ["WorstLatency_hls", "IntervalMax_hls", "FF_hls", "LUT_hls", "BRAM_18K_hls",
+                       "DSP_hls"]
+    binary_feature_names = ['hls_synth_success']
+    numeric_feature_names = ["d_in", "d_out", "prec", "rf", "WorstLatency_hls", "IntervalMax_hls", "FF_hls", "LUT_hls",
+                             "BRAM_18K_hls", "DSP_hls"]
+    categorical_feature_names = ["strategy"]
+
+    X, y, X_raw = preprocess('results/results_format_test.csv', input_features, output_features,
+                             binary_feature_names, numeric_feature_names,
+                             categorical_feature_names)
+
+
+    # Split the data 70 - 20 - 10 train test val
+    # Train and test
+    print(X.shape, y.shape)
+    print("X Data: ",input_features)
+    print(X)
+    print("Y Data: ",output_features)
+    print(y[0])
+    return sklearn.model_selection.train_test_split(X, y, X_raw,  test_size=0.2, random_state=42, shuffle=True)
+
+def train_classifier(X_train, y_train):
+
+    model = wa_hls4ml_model.create_model_classification()
+
+    adam = AdamW(learning_rate=0.001)
+    model.compile(optimizer=adam, loss=tf.keras.losses.Hinge(), metrics=['mse', 'mae'])
+    callbacks = all_callbacks(
+        stop_patience=1000,
+        lr_factor=0.5,
+        lr_patience=7,
+        lr_epsilon=0.000001,
+        lr_cooldown=2,
+        lr_minimum=0.00000001,
+        outputDir='model_class_1',
+    )
+    history = model.fit(
+        X_train,
+        y_train,
+        batch_size=256,
+        epochs=150,
+        validation_split=0.125,
+        shuffle=True,
+        callbacks=callbacks.callbacks,
+    )
+    plot_loss('classifier', history)
+
+
+def train_regressor(X_succeeded_train, y_succeeded_train):
+
+    model = wa_hls4ml_model.create_model_regression()
+
+    adam = AdamW(learning_rate=0.005)
+    model.compile(optimizer=adam, loss=tf.keras.losses.Huber(), metrics=['mse', 'mae'])
+    callbacks = all_callbacks(
+        stop_patience=1000,
+        lr_factor=0.5,
+        lr_patience=8,
+        lr_epsilon=0.0001,
+        lr_cooldown=3,
+        lr_minimum=0.0000000001,
+        outputDir='model_reg_1',
+    )
+    history = model.fit(
+        X_succeeded_train,
+        y_succeeded_train,
+        batch_size=128,
+        epochs=350,
+        validation_split=0.125,
+        shuffle=True,
+        callbacks=callbacks.callbacks,
+    )
+    plot_loss('regression',history)
+
+def display_results_classifier(X_test, X_raw_test, y_test, output_features):
+    
+    model = load_model('model_class_1/KERAS_check_best_model.h5')
+
+    # Predict the output for X_test
+    y_pred = model.predict(X_test)
+
+    # Calculate metrics
+    mae = mean_absolute_error(y_test, y_pred)
+    mse = mean_squared_error(y_test, y_pred)
+    rmse = np.sqrt(mse)
+    r2 = r2_score(y_test, y_pred)
+
+    y_pred_binary = np.where(y_pred > 1, 1, 0)
+    confusion = confusion_matrix(y_pred_binary, y_test)
+
+    print(f'Mean Absolute Error (MAE): {mae}')
+    print(f'Mean Squared Error (MSE): {mse}')
+    print(f'Root Mean Squared Error (RMSE): {rmse}')
+    print(f'R-squared (R2 Score): {r2}')
+    print('Confusion matrix:')
+    print(confusion)
+
+    y_test_2d = np.reshape(y_test, (y_test.shape[0], 1))
+
+    # plot our classification results
+    plot_results("classifier", False, y_test_2d, y_pred, X_raw_test, output_features)
+
+def display_results_regressor(X_succeeded_test, y_succeeded_test, X_raw_succeeded_test, output_features):
+
+    model = load_model('model_reg_1/KERAS_check_best_model.h5')
+
+    # Predict the output for X_test
+    y_pred = model.predict(X_succeeded_test)
+
+    # Calculate metrics
+    mae = mean_absolute_error(y_succeeded_test, y_pred)
+    mse = mean_squared_error(y_succeeded_test, y_pred)
+    rmse = np.sqrt(mse)
+    r2 = r2_score(y_succeeded_test, y_pred)
+
+    print(f'Mean Absolute Error (MAE): {mae}')
+    print(f'Mean Squared Error (MSE): {mse}')
+    print(f'Root Mean Squared Error (RMSE): {rmse}')
+    print(f'R-squared (R2 Score): {r2}')
+
+    # plot our regression results
+    plot_results("regression", False, y_succeeded_test, y_pred, X_raw_succeeded_test, output_features)
+
+if __name__ == "__main__":
+
+    train = False
+
+    # get raw data out
+    X_train, X_test, y_train, y_test, X_raw_train, X_raw_test = preprocess_data()
+
+    # separate the classification task data - remove the classification from input data
+    X_train_classifier = np.append(X_train[:, :4], X_train[:, 5:], axis=1)
+    X_test_classifier = np.append(X_test[:, :4], X_test[:, 5:], axis=1)
+    X_raw_test_classifier = np.append(X_raw_test[:, :4], X_raw_test[:, 5:], axis=1)
+
+    # get just the classification task as its own variable
+    y_train_classifier = X_train[:, 4]
+    y_test_classifier = X_test[:, 4]
+
+    # train the classifier
+    print("Training the classifier...")
+    # train_classifier(X_train_classifier, y_train_classifier)
+    display_results_classifier(X_test_classifier, X_raw_test_classifier, y_test_classifier, ["hls_synth_success"])
+
+    # find which synth has succeeded
+    succeeded_synth_gt_test = np.nonzero(y_test_classifier)
+    succeeded_synth_gt_train = np.nonzero(y_train_classifier)
+
+    # only test on successes
+    X_succeeded_test = X_test[succeeded_synth_gt_test]
+    y_succeeded_test = y_test[succeeded_synth_gt_test]
+    X_raw_succeeded_test = X_raw_test[succeeded_synth_gt_test]
+
+    # only train on successes
+    X_succeeded_train = X_train[succeeded_synth_gt_train]
+    y_succeeded_train = y_train[succeeded_synth_gt_train]
+    X_raw_succeeded_train = X_raw_train[succeeded_synth_gt_train, :]
+
+    # train the regressor
+    print("Training the regressor...")
+    # train_regressor(X_succeeded_train, y_succeeded_train)
+    display_results_regressor(X_succeeded_test, y_succeeded_test, X_raw_succeeded_test, ["WorstLatency_hls", "IntervalMax_hls", "FF_hls", "LUT_hls", "BRAM_18K_hls", "DSP_hls"])
+
+    print("Testing the models in union...")
+    model_classifier = load_model("model_class_1/KERAS_check_best_model.h5")
+
+    # predict the classes of the test dataset, convert to binary 1/0
+    class_pred = model_classifier.predict(X_test_classifier)
+    class_binary = np.where(class_pred > 1, 1, 0)
+    print(class_binary)
+    print(X_test.shape)
+
+    # insert the (wrong) binary classification to the middle of the input data
+    X_test_with_predicted_class = np.copy(X_test)
+    X_test_with_predicted_class[:, 4] = class_binary[0,:]
+
+    # get only the successes for prediction
+    succeeded_idx = np.nonzero(class_binary)[0]
+    print(succeeded_idx)
+    X_test_only_success = X_test_with_predicted_class[succeeded_idx]
+
+    model_regressor = load_model('model_reg_1/KERAS_check_best_model.h5')
+
+    # Predict the regression outputs for X_test
+    y_pred_without_class = model_regressor.predict(X_test_only_success)
+
+    # Add back the classification as a prediction index (does not yet need to have the data)
+    y_pred_with_class = np.append(y_pred_without_class, np.full((y_pred_without_class.shape[0], 1), 1), axis=1)
+
+    # Restore the indices that fail classification
+    y_pred_reshaped = np.empty((class_binary.shape[0], y_pred_with_class.shape[1]))
+    y_pred_reshaped[succeeded_idx] = y_pred_with_class
+    y_pred = np.where(class_binary > 0, y_pred_reshaped, -1)
+
+    # set the classification predictions as the 0/1 binary for error calc
+    y_pred[:, 6] = class_binary[0,:]
+    y_test_total = np.append(y_test, np.reshape(y_test_classifier, (y_test_classifier.shape[0], 1)), axis = 1)
+
+    print(y_pred.shape)
+    print(y_pred)
+
+    # Calculate metrics
+    mae = mean_absolute_error(y_test_total, y_pred)
+    mse = mean_squared_error(y_test_total, y_pred)
+    rmse = np.sqrt(mse)
+    r2 = r2_score(y_test_total, y_pred)
+
+    # now set classification predictions to be the hinge classification raw outputs
+    y_pred[:, 6] = class_pred[:,0]
+
+    print(f'Mean Absolute Error (MAE): {mae}')
+    print(f'Mean Squared Error (MSE): {mse}')
+    print(f'Root Mean Squared Error (RMSE): {rmse}')
+    print(f'R-squared (R2 Score): {r2}')
+
+    plot_results("both", False, y_test_total, y_pred, X_raw_test_classifier, ["WorstLatency_hls", "IntervalMax_hls", "FF_hls", "LUT_hls", "BRAM_18K_hls","DSP_hls","hls_synth_success"])
+
+    
+
+
+
+    
